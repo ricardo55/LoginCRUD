2a70e9d3b6b42eb8867937500e2d368acb4fb25f
Ricardo VB <pegassoric@hotmail.com>
2023-12-24 01:20:36 -0600

    Se agrego libraria redis

--- LoginCRUD/Program.cs
+++ LoginCRUD/Program.cs
@@ -10,17 +10,25 @@ using LoginCRUD.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'LoginDBContextConnection' not found.");
+// Funciona para crear la base de datos con postgresql en Azure
+builder.Services.AddDbContext<LoginDBContext>(options => options.UseNpgsql(connectionString));
+
+
+//var connectionString = builder.Configuration.GetConnectionString("LoginDBContextConnection") ?? throw new InvalidOperationException("Connection string 'LoginDBContextConnection' not found.");
 
 // Funciona para crear la base de datos con sqlite
 //builder.Services.AddDbContext<LoginDBContext>(options => options.UseSqlite(connectionString));
 
 // Funciona para crear la base de datos con postgresql
-builder.Services.AddDbContext<LoginDBContext>(options => options.UseNpgsql(connectionString));
+// builder.Services.AddDbContext<LoginDBContext>(options => options.UseNpgsql(connectionString));
+
+// Funciona para crear la BD Redis en Azure
 builder.Services.AddStackExchangeRedisCache(options =>{
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
+options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+options.InstanceName = "SampleInstance";
 });
 
+// Es para autenticar con el login de Identity
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<LoginDBContext>();
 
 // Add services to the container.
@@ -30,6 +38,7 @@ builder.Services.AddRazorPages();
 // builder.Services.AddDbContext<LoginDBContext2>(options =>
 //     options.UseSqlite(builder.Configuration.GetConnectionString("LoginDBContext2") ?? throw new InvalidOperationException("Connection string 'LoginDBContext2' not found.")));
 
+// Inicializar la base de datos con registros
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
